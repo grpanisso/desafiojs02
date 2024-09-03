fb0df1ca8f13777a581cb24b974957f58a5675ea
Gabriella Panisso <122941324+grpanisso@users.noreply.github.com>
2024-09-03 20:03:32 -0300

    Add files via upload

---
+++ desafio js.02/main.js
@@ -0,0 +1,96 @@
+//* 1. alert ("Hello World!")
+
+/* 2.
+let firstNumber = prompt ("Digite o promeiro número")
+let secondNumber = prompt ("Digite o segundo número")
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+
+const sum = firstNumber + secondNumber
+alert ("O resultado da soma dos dois números é de: " + sum)
+*/
+
+/* 3.
+let number = prompt ("Digite um número")
+
+number = Number(number)
+
+if (!isNaN(number)) {
+    alert ('É um número')
+} else {
+    alert ('Não é um número')
+}
+*/
+
+/*4.
+let input = prompt ("Digite algo")
+
+if (isNaN(input)) {
+    alert ('É uma string')
+} else {
+    alert ('Não é uma string')
+}
+*/
+
+ 
+/* 5.
+let value = true;
+
+if (typeof value ==='boolean') {
+    alert('É um booleano');
+    } else {
+        alert ('Não é um booleano')
+    }
+
+console.log (typeof value === 'boolean')
+*/
+
+/* 6.
+let numberOne = 5
+let numberTwo = 2
+
+const sub = (Number(numberOne) - Number(numberTwo))
+alert ("O resultado da subtração é de: " + sub)
+*/
+
+/* 7.
+let numberOne = 5
+let numberTwo = 2
+
+const multi = (Number(numberOne) * Number(numberTwo))
+alert ("O resultado da multiplicação é de: " + multi)
+*/
+
+
+/* 8.
+let numberOne = 5
+let numberTwo = 2
+const div = (Number(numberOne) / Number(numberTwo))
+alert ("O resultado da divisão é de: " + div)
+*/
+
+/* 9.
+let numberOne = prompt ("Digite um número")
+
+numberOne = Number(numberOne)
+
+if (numberOne % 2 === 0) {
+alert ("É um número par")
+} else {
+    alert ("Não é um número par")
+}
+    */
+
+/*
+let numberOne = prompt ("Digite um número")
+
+numberOne = Number(numberOne)
+
+if (numberOne % 2 ==! 0) {
+    alert ("É um número ímpar")
+    } else {
+        alert ("Não é um número ímpar")
+    }
+
+    */
